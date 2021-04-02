b3e2bccccb0214b39a2d76d00c633861ee2b016c
kwokkan <kwokkan@users.noreply.github.com>
2021-04-02 19:50:53 +0100

    Add install script.

--- install.sh
+++ install.sh
@@ -1,6 +1,29 @@
+
+cd ~/
+
+apt-get install -y git
+
 curl -fsSL https://deb.nodesource.com/setup_15.x | bash -
 apt-get install -y nodejs
+
+npm add -g yarn
+
+
+
+
+
+git clone --depth=1 https://github.com/kwokkan/blog-js.git ~/blog-js
+
+cd ~/blog-js/
+
+
+
+
 
 yarn install
 
 yarn export
+
+pkill busybox
+
+busybox httpd -p 0.0.0.0:80 -h out/
